a5050a8e424448fb3f944fa0d70d40045f67a6d6
Chandan Tiwadi <chandan110906@gmail.com>
2026-07-25 10:08:38 +0530

    Prac-8

---
+++ practical-8/src/App.jsx
@@ -0,0 +1,22 @@
+import React, {useState} from "react"
+
+
+function App(){
+  const [count, setCount] = useState(0);
+
+  return (
+    <div> 
+        <div>
+          <h2>Use Effect</h2>
+          <h2>{Count}</h2>
+          <div>
+            <button
+              classname=""
+            ></button>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default App;
